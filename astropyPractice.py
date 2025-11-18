21c0a6814a9ca923c2956f499e027ccd6690ebd5
India Daniel <72756858+igdaniel-1@users.noreply.github.com>
2025-11-18 12:26:49 -0500

    read from FITS file

--- astropyPractice.py
+++ astropyPractice.py
@@ -4,16 +4,23 @@ import matplotlib.pyplot as plt
 from astropy.visualization import astropy_mpl_style
 from astropy.utils.data import get_pkg_data_filename
 
-
+# style
 plt.style.use(astropy_mpl_style)
 
-image_file = get_pkg_data_filename('./M13.fits')
+# data
+image_file = get_pkg_data_filename('./M6707HH.fits')
+# print("\nimage file:", image_file)
 image_data = fits.getdata(image_file, ext=0)
 
-print(image_data.shape)
+# read data
+with fits.open('./M6707HH.fits') as hdul:
+    hdul.info()
+    print("\nHEADER:",hdul[0].header)
 
+print("\nSHAPE:",image_data.shape)
 
+# display
+plt.figure()
+plt.imshow(image_data)
+plt.colorbar()
 
-# with fits.open('./M13.fits') as hdul:
-#     hdul.info()
-#     print(hdul[0].header)
