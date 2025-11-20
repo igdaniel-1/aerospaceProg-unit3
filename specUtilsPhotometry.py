cb5824fbf0b5ec3e1be51b3a028023ae6ea4351a
India Daniel <72756858+igdaniel-1@users.noreply.github.com>
2025-11-20 14:10:57 -0500

    photometry demo with Photutils

--- specUtilsPhotometry.py
+++ specUtilsPhotometry.py
@@ -18,3 +18,35 @@ with fits.open(url) as hdulist:
     hdulist.info()
     data = hdulist[0].data
     header = hdulist[0].header
+
+unit = u.electron/ u.s
+mask = data == 0
+xdf_image = CCDData(data, unit=unit, meta=header, mask=mask)
+# Set up the figure with subplots
+fig, ax1 = plt.subplots(1, 1, figsize=(8, 8))
+# plt.show()
+
+# Set up the normalization and colormap
+norm_image = ImageNormalize(vmin=1e-4, vmax=5e-2, stretch=LogStretch(), clip=False)
+cmap = plt.get_cmap('plasma')
+cmap.set_over(cmap.colors[-1])
+cmap.set_under(cmap.colors[0])
+cmap.set_bad('white') # Show masked data as white
+xdf_image_clipped = np.clip(xdf_image, 1e-4, None) # clip to plot with logarithmic stretch
+
+# Plot the data
+fitsplot = ax1.imshow(np.ma.masked_where(xdf_image.mask, xdf_image_clipped), 
+                      norm=norm_image, cmap=cmap)
+
+# Define the colorbar and fix the labels
+cbar = plt.colorbar(fitsplot, fraction=0.046, pad=0.04, ticks=LogLocator(subs=range(10)))
+labels = ['$10^{-4}$'] + [''] * 8 + ['$10^{-3}$'] + [''] * 8 + ['$10^{-2}$']
+cbar.ax.set_yticklabels(labels)
+
+# Define labels
+cbar.set_label(r'Flux Count Rate ({})'.format(xdf_image.unit.to_string('latex')), 
+               rotation=270, labelpad=30)
+ax1.set_xlabel('X (pixels)')
+ax1.set_ylabel('Y (pixels)')
+
+plt.show()
